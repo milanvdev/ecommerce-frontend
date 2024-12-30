650b74d191ef9e6475cb8a730d8d945619872740
detrujamilan <milandetruja2@gmail.com>
2024-12-30 21:56:57 +0530

    lock file changes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), svgr()],
-    base: "/ecommerce-frontend"
+    base: "/ecommerce_frontend"
 })
